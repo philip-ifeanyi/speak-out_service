8250d810a830c9e056f3e25840090db1d25ea561
C0l0red <blaizepashcal@gmail.com>
2022-12-21 06:42:26 +0100

    Adds CORS header

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   await app.listen(process.env.PORT);
 }
 bootstrap();
